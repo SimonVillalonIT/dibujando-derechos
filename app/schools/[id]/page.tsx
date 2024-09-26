93039a269e2191b37a68a934ecef7446bce32d12
Tebbo <estebanromeropaez@gmail.com>
2024-09-26 11:22:27 -0300

    feat: supabase added

--- app/schools/[id]/page.tsx
+++ app/schools/[id]/page.tsx
@@ -4,10 +4,13 @@ import React from "react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 
+import { cn } from "@/lib/utils";
+import Close from "@/app/components/close";
 import SectionContainer from "@/app/components/section-container";
 
 function SchooldId() {
     const { id } = useParams<{ id: string }>();
+    const [drawModal, setDrawModal] = React.useState<number | null>(null);
     const imageUrls = [
         "https://picsum.photos/seed/1/300/200",
         "https://picsum.photos/seed/2/300/200",
@@ -59,73 +62,66 @@ function SchooldId() {
         "https://picsum.photos/seed/48/300/200",
         "https://picsum.photos/seed/49/300/200",
         "https://picsum.photos/seed/50/300/200",
-        "https://picsum.photos/seed/51/300/200",
-        "https://picsum.photos/seed/52/300/200",
-        "https://picsum.photos/seed/53/300/200",
-        "https://picsum.photos/seed/54/300/200",
-        "https://picsum.photos/seed/55/300/200",
-        "https://picsum.photos/seed/56/300/200",
-        "https://picsum.photos/seed/57/300/200",
-        "https://picsum.photos/seed/58/300/200",
-        "https://picsum.photos/seed/59/300/200",
-        "https://picsum.photos/seed/60/300/200",
-        "https://picsum.photos/seed/61/300/200",
-        "https://picsum.photos/seed/62/300/200",
-        "https://picsum.photos/seed/63/300/200",
-        "https://picsum.photos/seed/64/300/200",
-        "https://picsum.photos/seed/65/300/200",
-        "https://picsum.photos/seed/66/300/200",
-        "https://picsum.photos/seed/67/300/200",
-        "https://picsum.photos/seed/68/300/200",
-        "https://picsum.photos/seed/69/300/200",
-        "https://picsum.photos/seed/70/300/200",
-        "https://picsum.photos/seed/71/300/200",
-        "https://picsum.photos/seed/72/300/200",
-        "https://picsum.photos/seed/73/300/200",
-        "https://picsum.photos/seed/74/300/200",
-        "https://picsum.photos/seed/75/300/200",
-        "https://picsum.photos/seed/76/300/200",
-        "https://picsum.photos/seed/77/300/200",
-        "https://picsum.photos/seed/78/300/200",
-        "https://picsum.photos/seed/79/300/200",
-        "https://picsum.photos/seed/80/300/200",
-        "https://picsum.photos/seed/81/300/200",
-        "https://picsum.photos/seed/82/300/200",
-        "https://picsum.photos/seed/83/300/200",
-        "https://picsum.photos/seed/84/300/200",
-        "https://picsum.photos/seed/85/300/200",
-        "https://picsum.photos/seed/86/300/200",
-        "https://picsum.photos/seed/87/300/200",
-        "https://picsum.photos/seed/88/300/200",
-        "https://picsum.photos/seed/89/300/200",
-        "https://picsum.photos/seed/90/300/200",
-        "https://picsum.photos/seed/91/300/200",
-        "https://picsum.photos/seed/92/300/200",
-        "https://picsum.photos/seed/93/300/200",
-        "https://picsum.photos/seed/94/300/200",
-        "https://picsum.photos/seed/95/300/200",
-        "https://picsum.photos/seed/96/300/200",
-        "https://picsum.photos/seed/97/300/200",
-        "https://picsum.photos/seed/98/300/200",
-        "https://picsum.photos/seed/99/300/200",
-        "https://picsum.photos/seed/100/300/200",
     ];
+    const handleClose = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setDrawModal(null);
+    };
 
     return (
         <SectionContainer>
             <h1 className="text-6xl">{id}</h1>
             <div className="grid-gallery">
                 {imageUrls.map((img, i) => (
-                    <a className="grid-gallery__item" key={i} href="#">
+                    <div
+                        onClick={() => setDrawModal(i)}
+                        className="grid-gallery__item group relative cursor-pointer border-2 border-transparent hover:border-secondary"
+                        key={i}
+                    >
+                        <h6 className="absolute -right-4 -top-4 flex h-10 w-10 items-center justify-center rounded-full bg-secondary font-bold text-white">
+                            44
+                        </h6>
+                        {
+                            <div
+                                className={cn(
+                                    "fixed left-0 top-0 z-20 hidden h-screen w-full items-center justify-center bg-primary/60 backdrop-blur-md duration-200",
+                                    drawModal === i && "animate-fadeIn flex",
+                                )}
+                            >
+                                <div className="relative grid h-4/5 w-2/3 grid-cols-3 bg-background shadow-lg">
+                                    <Image
+                                        width={500}
+                                        height={500}
+                                        className="grid-gallery__image col-span-2"
+                                        src={img}
+                                        alt="Dibujo"
+                                    />
+                                    <div className="flex flex-col items-center justify-between px-4 py-16">
+                                        <div>
+                                            <h3 className="text-4xl">{id}</h3>
+                                            <h5 className="mt-8 text-xl">
+                                                Nombre de Alumno: Simon Villalon
+                                            </h5>
+                                        </div>
+                                        <button className="bg-secondary px-8 py-4 font-semibold duration-100 hover:scale-105">
+                                            Votar
+                                        </button>
+                                    </div>
+                                    <Close
+                                        onClick={handleClose}
+                                        className="absolute right-4 top-4 size-6 text-black"
+                                    />
+                                </div>
+                            </div>
+                        }
                         <Image
                             width={500}
                             height={500}
                             className="grid-gallery__image"
-                            key={i}
                             src={img}
                             alt="Dibujo"
                         />
-                    </a>
+                    </div>
                 ))}
             </div>
         </SectionContainer>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,18 +9,29 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            animation: { fadeIn: "fade-in ease-in-out 0.25s 1" },
+            keyframes: {
+                "fade-in": {
+                    "0%": {
+                        opacity: "0",
+                    },
+                    "100%": {
+                        opacity: "1",
+                    },
+                },
+            },
             colors: {
                 background: "#F8F8F8",
                 foreground: "#262626",
                 primary: "#595959",
                 secondary: "#01A9E0",
                 black: "#0D0D0D",
-                light: "#8C8C8C"
+                light: "#8C8C8C",
             },
             fontFamily: { sans: ["var(--poppins)", ...fontFamily.sans] },
             dropShadow: {
-        'red': '0 35px 35px #D6D6D6',
-      }
+                red: "0 35px 35px #D6D6D6",
+            },
         },
     },
     plugins: [],
